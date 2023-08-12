183384e71c20cbe6a1a0f9ab7a3d6a9ad48c4e58
shyammunde1 <shyammunde1@gmail.com>
2023-08-12 23:11:36 +0530

    changes on the input field

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -48,7 +48,7 @@ useTitle('User_SignUp')
             type="name"
             id="name"
             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-            placeholder="Shubham Sarda"
+            placeholder="samtech"
             required
             autoComplete="off"
           />
@@ -64,7 +64,7 @@ useTitle('User_SignUp')
             type="email"
             id="email"
             className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-            placeholder="shubham@example.com"
+            placeholder="samtech@example.com"
             required
             autoComplete="off"
           />
